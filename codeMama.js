586cec2dfcc855c402592882fad563d9d2dfe524
Masud Rana <msd.rana.2008@gmail.com>
2025-05-16 18:27:52 +0600

    Update codeMama.js

--- codeMama.js
+++ codeMama.js
@@ -1,20 +1,20 @@
-// This file is for personal practice
-// Celsius to Farenheit
+// // This file is for personal practice
+// // Celsius to Farenheit
 
 
-    // const celsius = parseInt(input);
+//     // const celsius = parseInt(input);
 
-    function celToFar (cel){
-        const Fahrenheit= (cel * 9/5 + 32).toFixed(2);
-        return Fahrenheit;
-    }
-    console.log("The temperature in Fahrenheit is: " + celToFar(32));
+//     function celToFar (cel){
+//         const Fahrenheit= (cel * 9/5 + 32).toFixed(2);
+//         return Fahrenheit;
+//     }
+//     console.log("The temperature in Fahrenheit is: " + celToFar(32));
 
 
-// const name = "John";
-// const age = 30;
-// const person = { name: "Jane", age: 25 };
+// // const name = "John";
+// // const age = 30;
+// // const person = { name: "Jane", age: 25 };
 
-// console.log("Name: %s, Age: %d", name, age);
-// console.log("Person object: %O", person);
-// console.log("%cStyled message", "color: blue; font-size: 20px;");
+// // console.log("Name: %s, Age: %d", name, age);
+// // console.log("Person object: %O", person);
+// // console.log("%cStyled message", "color: blue; font-size: 20px;");
