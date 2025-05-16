583ad7d739eefdab771219c513bceabc726371b1
Masud Rana <msd.rana.2008@gmail.com>
2025-05-16 17:51:43 +0600

    Create codeMama.js

---
+++ codeMama.js
@@ -0,0 +1,19 @@
+// Celsius to Farenheit
+
+
+    // const celsius = parseInt(input);
+
+    function celToFar (cel){
+        const Fahrenheit= (cel * 9/5 + 32).toFixed(2);
+        return Fahrenheit;
+    }
+    console.log("The temperature in Fahrenheit is: " + celToFar(32));
+
+
+// const name = "John";
+// const age = 30;
+// const person = { name: "Jane", age: 25 };
+
+// console.log("Name: %s, Age: %d", name, age);
+// console.log("Person object: %O", person);
+// console.log("%cStyled message", "color: blue; font-size: 20px;");
